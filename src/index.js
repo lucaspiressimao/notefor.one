7bcb3179268a4e4f47dec01547dbe788b66ac513
Lucas Pires Simão <lucaspiressimao@gmail.com>
2021-07-12 22:55:47 -0300

    Change mongo to read external

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "0.0.0.0";
 
-const memoryMongo = process.env.MEMORY_MONGO
+const externalMongo = process.env.EXTERNAL_MONGO
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,9 +26,9 @@ app.set("views", __dirname + "/views/");
 
 (async function () {
     
-    const mongoURI = memoryMongo ? await mongoServer.getUri() : process.env.MONGO_URL
-    const mongoDB = memoryMongo ? await mongoServer.getDbName() : process.env.MONGO_DB
-    
+    const mongoURI = externalMongo ? process.env.MONGO_URL : (await mongoServer.getUri())
+    const mongoDB = externalMongo ? process.env.MONGO_DB : (await mongoServer.getDbName())
+
     MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
         if (err) return console.error(err);
         app.listen(port, host, async () => {
